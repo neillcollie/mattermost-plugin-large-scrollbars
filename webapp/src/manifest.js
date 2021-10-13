66f4b43263860908395eafa88d87fdde972023c4
neill <neillcollie@gmail.com>
2021-10-13 20:16:50 +0100

    Updated build number and description

--- webapp/src/manifest.js
+++ webapp/src/manifest.js
@@ -4,8 +4,8 @@ const manifest = JSON.parse(`
 {
     "id": "com.mattermost.plugin-ui-customise",
     "name": "Customise UI Elements",
-    "description": "Plugin to remove 'files' button and search tip",
-    "version": "0.2.0",
+    "description": "Plugin to remove 'files' button and from Header and Search Bar",
+    "version": "0.2.1",
     "min_server_version": "5.31.0",
     "webapp": {
         "bundle_path": "webapp/dist/main.js"
